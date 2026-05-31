8ce1180d03195ccf0af810cab2d5c466be986e1b
jgeringer <joegeringer@gmail.com>
2026-05-30 22:55:09 -0500

    init portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    // 👇 update these lines
+    sanity({
+      projectId: "98il5c52",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
